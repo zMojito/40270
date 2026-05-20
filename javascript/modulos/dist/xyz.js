bd4b6fa23242a2849a2369fc0176b0508989fc97
zMojito <emilianohdzga@gmail.com>
2026-05-20 16:54:29 -0600

    produccion js

---
+++ javascript/modulos/dist/xyz.js
@@ -0,0 +1,2 @@
+(()=>{function n(o){return console.log("y:"+o),"fin y"}function l(o){return n("x"),console.log("x:"+o),"fin x"}function r(o){return"fin xx"}function x(o){return console.log("z:"+o),"fin z"}console.log(l("abc"));console.log(r("def"));console.log(l("abc"));console.log(r("def"));console.log(n("ghi"));console.log(x("jkl"));})();
+//# sourceMappingURL=xyz.js.map
